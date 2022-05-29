fe7bb9c35927cc703da0bfc45e9106ff155a0c62
Aindrian2202 <domashovets@smarttender.biz>
2022-05-29 17:28:03 +0300

    update label

--- frontity.settings.js
+++ frontity.settings.js
@@ -3,7 +3,7 @@ const settings = {
   "state": {
     "frontity": {
       "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
+      "title": "Test Themes 7friday",
       "description": "WordPress installation for Frontity development"
     }
   },
