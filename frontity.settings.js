6d42cf7c256c8e4e04bc58b9268d9241b10f909c
Andrian <59098456+ynherb@users.noreply.github.com>
2022-05-29 16:10:51 +0300

    Add files via upload

---
+++ frontity.settings.js
@@ -0,0 +1,76 @@
+const settings = {
+  "name": "7fridays",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development",
+    },
+  },
+  "packages": [
+    {
+      "name": "@frontity/twentytwenty-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "http://frontitytest.local",
+          "postTypes": [
+            {
+              type: "result",
+              endpoint: "result",
+              archive: '/archives/result',
+            },
+            {
+              type: "product",
+              endpoint: "product",
+              archive: '/product',
+            }
+          ],
+          "taxonomies": [
+            {
+              taxonomy: "category", // taxonomy slug
+              endpoint: "categories", // REST API endpoint
+              postTypeEndpoint: "result", // endpoint from which posts from this taxonomy are fetched
+            }
+          ]
+        },
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
